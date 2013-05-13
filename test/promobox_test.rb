0bc61b493060d6b7e7022f3c26dc5b61406cb98f
Laurent Arnoud <larnoud@af83.com>
2013-05-13 19:08:03 +0200

    Spec: added shops with lat long params

--- test/promobox_test.rb
+++ test/promobox_test.rb
@@ -8,9 +8,11 @@ describe Promobox do
     @promobox = Promobox.new('apikey', 'demo@atipik.fr', 'password')
   end
 
-  def promobox_stub(action)
-    fixture_file = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures', "#{action}.json"))
-    url = @promobox.send(:build_query, action, {})
+  def promobox_stub(action, params={})
+    file = "#{action}.json"
+    file = "#{action}_#{URI.encode_www_form params}.json" unless params.empty?
+    fixture_file = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures', file))
+    url = @promobox.send(:build_query, action, params)
     stub(OpenURI::OpenRead).read(url) { File.read fixture_file }
   end
 
@@ -44,17 +46,35 @@ describe Promobox do
   end
 
   describe 'shops' do
-    before do
-      promobox_stub('shops')
+    describe 'without params' do
+      before do
+        promobox_stub('shops')
+      end
+
+      it 'should respond empty data' do
+        shops = @promobox.shops
+        shops.must_be_instance_of Hash
+        shops['data'].must_be_empty
+        shops['current_page'].must_equal 1
+        shops['total_page'].must_equal 0
+        shops['total_item'].must_equal 0
+      end
     end
 
-    it 'should respond' do
-      shops = @promobox.shops
-      shops.must_be_instance_of Hash
-      shops['data'].must_be_empty
-      shops['current_page'].must_equal 1
-      shops['total_page'].must_equal 0
-      shops['total_item'].must_equal 0
+    describe 'with params' do
+      before do
+        @params = {lat: '49.824837', lon: '3.280160'}
+        promobox_stub('shops', @params)
+      end
+
+      it 'should respond' do
+        shops = @promobox.shops(@params)
+        shops.must_be_instance_of Hash
+        shops['data'].wont_be_empty
+        shops['current_page'].must_equal 1
+        shops['total_page'].must_equal 260
+        shops['total_item'].must_equal 5187
+      end
     end
   end
 end
